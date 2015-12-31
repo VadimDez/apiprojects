0a0c85008de8a2902744912943537b2258daa293
Vadym Yatsyuk <vasp3d@gmail.com>
2015-12-31 22:17:22 +0100

    removed unused

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,18 +1,12 @@
 var express = require('express'),
-  router = express.Router(),
-  mongoose = require('mongoose'),
-  Article = mongoose.model('Article');
+  router = express.Router();
 
 module.exports = function (app) {
   app.use('/', router);
 };
 
 router.get('/', function (req, res, next) {
-  Article.find(function (err, articles) {
-    if (err) return next(err);
     res.render('index', {
-      title: 'Generator-Express MVC',
-      articles: articles
+      title: 'APi project'
     });
-  });
 });
